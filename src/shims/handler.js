b79c948af5dfb70a279b53763ea3e0d7b979d1bd
yugasun <yuga_sun@163.com>
2020-05-14 20:32:45 +0800

    fix: custom sls.js load logical

--- src/shims/handler.js
+++ src/shims/handler.js
@@ -1,19 +1,23 @@
 const path = require('path')
 const fs = require('fs')
 const { createServer, proxy } = require('tencent-serverless-http')
-let app = require('./sls')
 
 module.exports.handler = async (event, context) => {
   const userSls = path.join(__dirname, 'sls.js')
+  let app
   if (fs.existsSync(userSls)) {
     // eslint-disable-next-line
     console.log('Using user custom sls.js')
     app = require(userSls)
+  } else {
+    app = require('./sls')
   }
 
   context.callbackWaitsForEmptyEventLoop =
     app.callbackWaitsForEmptyEventLoop === true ? true : false
 
-  const server = createServer(app.callback(), null, app.binaryTypes || [])
-  return proxy(server, event, context, 'PROMISE').promise
+  if (!global.server) {
+    global.server = createServer(app.callback(), null, app.binaryTypes || [])
+  }
+  return proxy(global.server, event, context, 'PROMISE').promise
 }
