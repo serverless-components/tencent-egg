3b69e7d6ca270b5a46bcb1cb31cb6be584d1a4fb
Avril Li <linxforever@vip.qq.com>
2020-07-03 15:06:39 +0800

    Add integration test (#14)
    * chore(test): add test and update isDisabled config
    
    * chore: update jest version to support node8.x
    
    * chore: remove process.env variables of test
    
    * chore: update test case
    
    * chore: update test region
    
    * chore: update test region and timeout
    
    * chore: update test case

---
+++ tests/integration.test.js
@@ -0,0 +1,53 @@
+const { generateId, getServerlessSdk } = require('./utils')
+const execSync = require('child_process').execSync
+const path = require('path')
+const axios = require('axios')
+
+// set enough timeout for deployment to finish
+jest.setTimeout(600000)
+
+// the yaml file we're testing against
+const instanceYaml = {
+  org: 'orgDemo',
+  app: 'appDemo',
+  component: 'egg@dev',
+  name: `egg-integration-tests-${generateId()}`,
+  stage: 'dev',
+  inputs: {
+    region: 'ap-guangzhou',
+    runtime: 'Nodejs8.9',
+    apigatewayConf: { environment: 'test' }
+  }
+}
+
+// get credentials from process.env but need to init empty credentials object
+const credentials = {
+  tencent: {}
+}
+
+// get serverless construct sdk
+const sdk = getServerlessSdk(instanceYaml.org)
+
+it('should successfully deploy egg app', async () => {
+  const instance = await sdk.deploy(instanceYaml, { tencent: {} })
+
+  expect(instance).toBeDefined()
+  expect(instance.instanceName).toEqual(instanceYaml.name)
+  // get src from template by default
+  expect(instance.outputs.templateUrl).toBeDefined()
+  expect(instance.outputs.region).toEqual(instanceYaml.inputs.region)
+  expect(instance.outputs.apigw).toBeDefined()
+  expect(instance.outputs.apigw.environment).toEqual(instanceYaml.inputs.apigatewayConf.environment)
+  expect(instance.outputs.scf).toBeDefined()
+  expect(instance.outputs.scf.runtime).toEqual(instanceYaml.inputs.runtime)
+
+  const response = await axios.get(instance.outputs.apigw.url)
+  expect(response.data.includes('Hello Egg.js')).toBeTruthy()
+})
+
+it('should successfully remove egg app', async () => {
+  await sdk.remove(instanceYaml, credentials)
+  result = await sdk.getInstance(instanceYaml.org, instanceYaml.stage, instanceYaml.app, instanceYaml.name)
+
+  expect(result.instance.instanceStatus).toEqual('inactive')
+})
